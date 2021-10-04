cd943d365b7d28fd244bbe23d58340b9a5f6e906
Brian Alvey <github@subs.frankensite.com>
2021-10-03 19:54:56 -0700

    intro screen design

--- assets/theme.js
+++ assets/theme.js
@@ -3,7 +3,7 @@ const thisTag = urlParams.get('tag');
 
 window.onload = function() {
     // document.getElementById("introHostName").innerHTML = "Fabletics &#8212; " + thisTag;
-    document.getElementById("introTitle").innerText = "Send us a video! Tell us what you love, what you don't love or ask a question.";
+    document.getElementById("introTitle").innerHTML = "Send us a video!<br />Tell us what you love, what you don't love or ask us a question.";
     // document.getElementById("introHostName").style.display = "block";
     document.getElementById("introTitle").style.display = "block";
 };
