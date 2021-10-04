01d675b6af4a027c8bbd3efa94e223aedd0047e3
Brian Alvey <github@subs.frankensite.com>
2021-10-03 18:27:43 -0700

    Update theme.js

--- assets/theme.js
+++ assets/theme.js
@@ -1,5 +1,4 @@
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("introHostName").innerText = "Frank";
     document.getElementById("introTitle").innerText = "Oh, show me the way to the next whiskey bar.";
-};
-  
+}, false);
