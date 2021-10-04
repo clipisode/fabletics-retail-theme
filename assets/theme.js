e4e33fb1b13f583e98419c4e01347311ef5264bc
Brian Alvey <github@subs.frankensite.com>
2021-10-03 19:52:39 -0700

    Update theme.js

--- assets/theme.js
+++ assets/theme.js
@@ -2,8 +2,8 @@ const urlParams = new URLSearchParams(window.location.search);
 const thisTag = urlParams.get('tag');
 
 window.onload = function() {
-    document.getElementById("introHostName").innerHTML = "Fabletics &#8212; " + thisTag;
+    // document.getElementById("introHostName").innerHTML = "Fabletics &#8212; " + thisTag;
     document.getElementById("introTitle").innerText = "Send us a video! Tell us what you love, what you don't love or ask a question.";
-    document.getElementById("introHostName").style.display = "block";
+    // document.getElementById("introHostName").style.display = "block";
     document.getElementById("introTitle").style.display = "block";
 };
