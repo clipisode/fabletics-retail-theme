2523f46c037010c71a36b3362107aa6491019ae0
Brian Alvey <github@subs.frankensite.com>
2021-10-03 18:35:38 -0700

    no content flashing

--- assets/theme.js
+++ assets/theme.js
@@ -1,4 +1,6 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.onload = function() {
     document.getElementById("introHostName").innerText = "Frank";
     document.getElementById("introTitle").innerText = "Oh, show me the way to the next whiskey bar.";
-});
+    document.getElementById("introHostName").style.display = "block";
+    document.getElementById("introTitle").style.display = "block";
+};
