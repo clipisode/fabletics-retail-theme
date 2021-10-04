1835d917d2a2943e386ffb40aa9c25049b0bfc82
Brian Alvey <github@subs.frankensite.com>
2021-10-03 21:53:15 -0700

    image cycling

--- assets/theme.js
+++ assets/theme.js
@@ -1,9 +1,16 @@
-const urlParams = new URLSearchParams(window.location.search);
-const thisTag = urlParams.get('tag');
+// const urlParams = new URLSearchParams(window.location.search);
+// const thisTag = urlParams.get('tag');
 
 window.onload = function() {
     // document.getElementById("introHostName").innerHTML = "Fabletics &#8212; " + thisTag;
     document.getElementById("introTitle").innerHTML = "<p>Send us a video review.</p><p>Tell us what you love, what you don't love or ask us a question.</p>";
     // document.getElementById("introHostName").style.display = "block";
     document.getElementById("introTitle").style.display = "block";
+    var noVideoArea = document.getElementById("noVideoArea");
+    var img1 = document.createElement('img');
+    img1.src = './retail-000.png';
+    img1.className = "object-cover w-full h-full opacity-0";
+    noVideoArea.appendChild(img1);
 };
+
+// <img src="https://d22rmu8ky1z7ab.cloudfront.net/615a845fcb7a28d9e5e939e0/retail-002.png" class="object-cover w-full h-full">
