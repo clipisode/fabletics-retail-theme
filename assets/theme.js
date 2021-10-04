04466ff07b92778a76278457dcc419a6b27a0a43
Brian Alvey <github@subs.frankensite.com>
2021-10-03 22:24:14 -0700

    image work

--- assets/theme.js
+++ assets/theme.js
@@ -1,21 +1,18 @@
 // const urlParams = new URLSearchParams(window.location.search);
 // const thisTag = urlParams.get('tag');
 
+const noVideoArea = document.getElementById("noVideoArea");
+
+function addImg(arg) {
+    var thisDiv = document.createElement('div');
+    thisDiv.className = "img"+arg+" object-cover w-full h-full";
+    noVideoArea.appendChild(thisDiv);
+}
+
 window.onload = function() {
     // document.getElementById("introHostName").innerHTML = "Fabletics &#8212; " + thisTag;
     document.getElementById("introTitle").innerHTML = "<p>Send us a video review.</p><p>Tell us what you love, what you don't love or ask us a question.</p>";
     // document.getElementById("introHostName").style.display = "block";
     document.getElementById("introTitle").style.display = "block";
-    var noVideoArea = document.getElementById("noVideoArea");
-    var img0 = document.createElement('div');
-    img0.className = "img0 object-cover w-full h-full";
-    noVideoArea.appendChild(img0);
-    var img1 = document.createElement('div');
-    img1.className = "img1 object-cover w-full h-full";
-    noVideoArea.appendChild(img1);
-    var img2 = document.createElement('div');
-    img2.className = "img2 object-cover w-full h-full";
-    noVideoArea.appendChild(img2);
+    addImg(0); addImg(1); addImg(2); addImg(3); addImg(4);
 };
-
-// <img src="https://d22rmu8ky1z7ab.cloudfront.net/615a845fcb7a28d9e5e939e0/retail-002.png" class="object-cover w-full h-full">
